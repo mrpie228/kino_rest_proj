0a8cd6764c6fb14417aa4268b5917b2b4c7104af
Derah Valentin <41872026+mrpie228@users.noreply.github.com>
2021-01-24 14:15:55 +0300

    f

--- kino/frontend/vue-fronterd/src/views/Search.vue
+++ kino/frontend/vue-fronterd/src/views/Search.vue
@@ -1,5 +1,14 @@
 <template>
+<section class="main-container" >
   <div class="search">
     <h1>This is an search page</h1>
+    <form >
+      <input v-model="message" placeholder="Search something..." />
+      <button>sumbit</button><br>
+      <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />
+      
+    </form>
+    <h2>{{message}}</h2>
   </div>
+</section>
 </template>
